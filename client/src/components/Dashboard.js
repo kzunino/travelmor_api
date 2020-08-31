13c5b8efca48f8b9b63f1935a473a0bdbe584b8d
Kyle <kzunino@gmail.com>
2020-08-31 11:47:57 -0700

    Working on the tables

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -8,6 +8,18 @@ import Box from '@material-ui/core/Box';
 import Divider from '@material-ui/core/Divider';
 import Button from '@material-ui/core/Button';
 
+// Table
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+//Charts
+
 import {
   BarChart,
   Bar,
@@ -27,10 +39,25 @@ import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 const drawerWidth = 240;
 
+function createData(name, calories, fat, carbs, protein) {
+  return {name, calories, fat, carbs, protein};
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
 const useStyles = makeStyles((theme) => ({
   toolbarMargin: {
     ...theme.mixins.toolbar,
     marginBottom: '1em',
+  },
+  table: {
+    minWidth: 650,
   },
   toolbar: {
     padding: 0,
@@ -108,6 +135,34 @@ const Dashboard = () => {
   const classes = useStyles();
 
   const matchXs = useMediaQuery(theme.breakpoints.down('xs'));
+
+  //if type is null then uncategorized
+  const expenses = [
+    {
+      type: 'Transportation',
+      name: 'bus tickets',
+      cost: 12.0,
+      date: Date.now(),
+    },
+    {
+      type: 'Food',
+      name: 'breakfast',
+      cost: 42.0,
+      date: Date.now(),
+    },
+    {
+      type: 'Tour',
+      name: 'Cusco City Tour',
+      cost: 50.0,
+      date: Date.now(),
+    },
+    {
+      type: 'Gifts',
+      name: 'Bottle of Pisco',
+      cost: 18.0,
+      date: Date.now(),
+    },
+  ];
 
   const data = [
     {
@@ -401,7 +456,7 @@ const Dashboard = () => {
                         data={pieData}
                         cx={150}
                         cy={150}
-                        labelLine={false}
+                        labelLine={true}
                         label={renderCustomizedLabel}
                         outerRadius={80}
                         fill='#8884d8'
@@ -422,6 +477,52 @@ const Dashboard = () => {
                         <Typography variant='h4' align='center'>
                           Expense History
                         </Typography>
+                        <Grid item>
+                          {' '}
+                          <TableContainer component={Paper}>
+                            <Table
+                              className={classes.table}
+                              aria-label='simple table'
+                            >
+                              <TableHead>
+                                <TableRow>
+                                  <TableCell>Dessert (100g serving)</TableCell>
+                                  <TableCell align='right'>Calories</TableCell>
+                                  <TableCell align='right'>
+                                    Fat&nbsp;(g)
+                                  </TableCell>
+                                  <TableCell align='right'>
+                                    Carbs&nbsp;(g)
+                                  </TableCell>
+                                  <TableCell align='right'>
+                                    Protein&nbsp;(g)
+                                  </TableCell>
+                                </TableRow>
+                              </TableHead>
+                              <TableBody>
+                                {rows.map((row) => (
+                                  <TableRow key={row.name}>
+                                    <TableCell component='th' scope='row'>
+                                      {row.name}
+                                    </TableCell>
+                                    <TableCell align='right'>
+                                      {row.calories}
+                                    </TableCell>
+                                    <TableCell align='right'>
+                                      {row.fat}
+                                    </TableCell>
+                                    <TableCell align='right'>
+                                      {row.carbs}
+                                    </TableCell>
+                                    <TableCell align='right'>
+                                      {row.protein}
+                                    </TableCell>
+                                  </TableRow>
+                                ))}
+                              </TableBody>
+                            </Table>
+                          </TableContainer>
+                        </Grid>
                       </Grid>
                     </Grid>
                   </Grid>
