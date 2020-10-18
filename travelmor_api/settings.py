ec692cb69680d788b7578a32e774dfe8f2a503c4
Kyle <kzunino@gmail.com>
2020-10-17 22:51:05 -0700

    changed debug

--- travelmor_api/settings.py
+++ travelmor_api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DISABLE_COLLECTSTATIC = 1
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ["*"]
 ALLOWED_HOSTS = ["travelmor-api.herokuapp.com"]
