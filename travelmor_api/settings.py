64a12c08aea1a7e746d5c49c3f21c6afb5676c00
Kyle <kzunino@gmail.com>
2020-10-17 23:31:04 -0700

    whitenoise added

--- travelmor_api/settings.py
+++ travelmor_api/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# ALLOWED_HOSTS = ["*"]
-ALLOWED_HOSTS = ["travelmor-api.herokuapp.com"]
+ALLOWED_HOSTS = ["*"]
+# ALLOWED_HOSTS = ["travelmor-api.herokuapp.com"]
 
 
 # Application definition
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
